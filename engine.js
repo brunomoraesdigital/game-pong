512ca06aa4740e019e64bdaecdfe23d9dfbbf7c4
Bruno Moraes <89628890+brunomoraesdigital@users.noreply.github.com>
2025-03-18 20:20:50 +0000

    180325.1720

--- engine.js
+++ engine.js
@@ -523,8 +523,8 @@ const anoAtualElement = document.getElementById("ano-atual");
 const anoAtual = new Date().getFullYear();
 
 // Atualiza o conteúdo do elemento com o ano atual
-anoAtualElement.textContent = `Copyright ©
-${anoAtual} Bruno Moraes`;
+anoAtualElement.textContent = `©
+${anoAtual} Bruno Moraes - Evoluindo a cada código | Licença AGPL v3`;
 /*********************************/
 
 
